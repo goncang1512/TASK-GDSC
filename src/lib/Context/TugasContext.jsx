1e5632c5cf15a608a2003541bbadd5ac0c5189e8
goncang1512 <samuderanst@gmail.com>
2024-03-01 13:55:46 +0700

    fix list sort data

--- src/lib/Context/TugasContext.jsx
+++ src/lib/Context/TugasContext.jsx
@@ -67,9 +67,24 @@ export default function TugasContextProvider({ children }) {
   const getTugas = useCallback(() => {
     try {
       const response = JSON.parse(localStorage.getItem("listTugas"));
-      const result = response.sort(
-        (a, b) => new Date(a.deadLine) - new Date(b.deadLine)
+      const sortedTasks = response.sort((a, b) => {
+        if (a.status === "completed" && b.status !== "completed") {
+          return 1;
+        }
+        if (b.status === "completed" && a.status !== "completed") {
+          return -1;
+        }
+        return new Date(a.deadLine) - new Date(b.deadLine);
+      });
+
+      const completedTasks = sortedTasks.filter(
+        (task) => task.status === "completed"
       );
+      const incompletedTasks = sortedTasks.filter(
+        (task) => task.status !== "completed"
+      );
+
+      const result = [...incompletedTasks, ...completedTasks];
       setMyTask(result);
     } catch (error) {
       console.log(error);
